2ea34fcf290782a9d959ba0ae0f72b4e1068f33c
Guy Altman <guyaltman98@gmail.com>
2023-03-08 00:00:37 -0500

    captured email datetime and pdf invoice text

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+import pprint as pp
+import pytz
+import datetime
+import base64
+from io import BytesIO
+import PyPDF2
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -45,32 +51,45 @@ def get_invoice_pdf_list():
 
         print(f"Number of messages sent from djoy@portconsolidated.com: {len(messages)}")
 
-        for msg in messages:
+        for message in messages:
             # Get the message by ID
-            message = service.users().messages().get(userId='me', id=msg['id']).execute()
+            message = service.users().messages().get(userId='me', id=message['id']).execute()
+            timestamp = int(message['internalDate']) / 1000.0
+            email_datetime_recieved = datetime.datetime.fromtimestamp(timestamp)
+            
+            # Set EST timezone
+            est_tz = pytz.timezone('US/Eastern')
+
+            # Convert datetime object to EST timezone
+            est_datetime = email_datetime_recieved.astimezone(est_tz)
+            #pp.pprint(est_datetime)
+            #print(est_datetime.strftime("%Y-%m-%d %H:%M:%S"))
+
+            # pp.pprint(message)
+            # pp.pprint(dir(message))
 
             # Get the payload of the message (including the headers and body)
             payload = message['payload']
 
             # Iterate over the parts of the payload (which could include email body and/or attachments)
-            for part in payload['parts']:
-                # Check if the part contains an attachment
-                if part.get('filename') and part.get('body') and part.get('body').get('attachmentId'):
-                    # Get the attachment by ID
-                    attachment = service.users().messages().attachments().get(
-                        userId='me', messageId=message['id'], id=part['body']['attachmentId']).execute()
-
-                    # Decode the attachment data (which is in base64)
-                    data = attachment['data']
-                    decoded_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
-
-                    # Save the attachment to a file
-                    with open(part['filename'], 'wb') as f:
-                        f.write(decoded_data)
-                        print(f"Attachment saved to file: {part['filename']}")
-                        break
-            
-            break
+            for part in message['payload']['parts']:
+                if part['filename'] and part['filename'].endswith('.pdf'):
+                    data = part['body'].get('data')
+                    if not data:
+                        att_id = part['body'].get('attachmentId')
+                        att = service.users().messages().attachments().get(userId='me', messageId=message['id'], id=att_id).execute()
+                        data = att['data']
+                    pdf_bytes = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                    pdf_file = BytesIO(pdf_bytes)
+                    pdf_reader = PyPDF2.PdfReader(pdf_file)
+                    text = ''
+                    for page in range(1):
+                        text += pdf_reader.pages[page].extract_text()
+                    new = text.split('\n')
+                    print(new[8])
+                    #break
+            #break in place so that loop only executes once
+            #break
                         
     except HttpError as error:
         print(f'An error occurred: {error}')
