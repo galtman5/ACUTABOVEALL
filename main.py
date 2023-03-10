455a651b504f6b0cd997e106af3fe4eb5699118b
Guy Altman <guyaltman98@gmail.com>
2023-03-10 01:22:30 -0500

    moved similar functions into methods of a class

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from io import BytesIO
 from prefect.blocks.system import Secret
 from snowflake.connector.pandas_tools import write_pandas
+from invoice_pdf_class import InvoicePdf
 import base64
 import pprint as pp
 import base64
 import os.path
 import PyPDF2
-import re
 import pandas as pd
 import snowflake.connector
 
@@ -79,8 +79,11 @@ def get_invoice_pdf_list():
 
                     # invoice is only on the first page of the pdf email attachment
                     page_text = pdf_reader.pages[0].extract_text()
-                    invoice_data_payload = extract_data_from_pdf(page_text)
-                    write_to_snowflake(invoice_data_payload)
+                    invoice_pdf = InvoicePdf(page_text)
+                    print(invoice_pdf)
+
+                    # backfill has already happened. Need to implement the checker flow now.
+                    #write_to_snowflake(invoice_data_payload)
 
     except HttpError as error:
         print(f'An error occurred: {error}')
@@ -108,81 +111,7 @@ def write_to_snowflake(json_payload):
     print(res)
 
 
-def extract_invoice_id(invoice):
-    pattern = r"(?i)(?<=Invoice Number[:;]\s)[A-Z0-9]+"
-    try:
-        invoice_id = re.search(pattern, invoice).group(0)
-        return invoice_id
-    except AttributeError:
-        return None
 
-def extract_invoice_datetime(invoice):
-    pattern = r"(?i)(?<=Invoice Date[:;]\s)\d{2}/\d{2}/\d{2}"
-    try:
-        invoice_datetime = re.search(pattern, invoice).group(0)
-        return invoice_datetime
-    except AttributeError:
-        return None
-
-def extract_invoice_amount_due(invoice):
-    pattern = r"(?i)(?:Ampunt|Amount)\s*Due[;:]\s*\$\s*(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)"
-    try:
-        invoice_amnt_due = re.search(pattern, invoice).group(1)
-        invoice_amnt_due = invoice_amnt_due.replace(',', '')
-        return float(invoice_amnt_due)
-    except AttributeError:
-        return None
-
-def extract_invoice_due_date(invoice):
-    pattern = r"(?<=Due Date[:;] )\d{2}\/\d{2}\/\d{2}"
-    try:
-        invoice_amnt_due = re.search(pattern, invoice).group(0)
-        return invoice_amnt_due
-    except AttributeError:
-        return None
-
-def extract_gas_quantity(invoice):
-    pattern = r"(?<!\.)\b\d+\.\d{4}\b(?!\.)"
-    try:
-        invoice_amnt_due = re.search(pattern, invoice).group(0)
-        return invoice_amnt_due
-    except AttributeError:
-        return None
-
-def extract_driver(invoice):
-    pattern = r"Driver[:;]? (\w+\s+\w+)"
-    try:
-        invoice_amnt_due = re.search(pattern, invoice).group(0)
-        return invoice_amnt_due
-    except AttributeError:
-        return None
-
-# experimental
-def extract_unit_price(invoice):
-    pattern = r"(?<!\.)\b\d+\.\d{4}\b\s*\$(\d{1,3}(,\d{3})*(\.\d+)?)\b"
-    try:
-        invoice_amnt_due = re.search(pattern, invoice).group(1)
-        return invoice_amnt_due
-    except AttributeError:
-        return invoice
-
-def extract_data_from_pdf(pdf):
-    invoice_id = extract_invoice_id(pdf)
-    invoice_datetime = extract_invoice_datetime(pdf)
-    invoice_amnt_due = extract_invoice_amount_due(pdf)
-    invoice_due_date = extract_invoice_due_date(pdf)
-    gas_quantity = extract_gas_quantity(pdf)
-    driver = extract_driver(pdf)
-    #unit_price = extract_unit_price(pdf)
-
-    return {
-        'INVOICE_ID': [invoice_id],
-        'INVOICE_DATE': [invoice_datetime],
-        'INVOICE_GAS_QUANTITY': [gas_quantity],
-        'INVOICE_AMOUNT_DUE': [invoice_amnt_due],
-        'INVOICE_DUE_DATE': [invoice_due_date],
-        'DRIVER': [driver]
-        }
 
 
 if __name__ == '__main__':
